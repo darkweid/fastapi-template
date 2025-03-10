b400fa47f2f5220eaf6447911f770f1bf502957e
Mark Shamanskii <darkweid713@gmail.com>
2025-03-10 22:26:36 +0500

    Feat: Add settings configuration and connection builders for PostgreSQL, Redis, and RabbitMQ

---
+++ app/core/settings.py
@@ -0,0 +1,72 @@
+from pydantic_settings import BaseSettings
+from sqlalchemy import URL
+
+
+class Settings(BaseSettings):
+    sentry_dsn: str
+    db_echo: bool
+    project_name: str
+    version: str
+    debug: bool
+    cors_allowed_origins: str
+
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
+
+    jwt_user_secret_key: str
+    jwt_admin_secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+    refresh_token_expire_minutes: int
+
+    redis_host: str
+    redis_port: int
+    redis_password: str
+    redis_database: int = 0
+
+    rabbitmq_host: str
+    rabbitmq_port: int
+    rabbitmq_user: str
+    rabbitmq_password: str
+
+    bucket_name: str
+    aws_access_key_id: str
+    aws_secret_access_key: str
+    region_name: str
+    s3_sample_url: str
+
+    def build_postgres_dsn_async(self) -> URL:
+        return URL.create(
+            "postgresql+asyncpg",
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            port=self.postgres_port,
+            database=self.postgres_db,
+        )
+
+    def build_postgres_dsn_sync(self) -> URL:
+        return URL.create(
+            "postgresql",
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            port=self.postgres_port,
+            database=self.postgres_db,
+        )
+
+    def build_redis_dsn(self) -> str:
+        return f"redis://:{self.redis_password}@{self.redis_host}:{self.redis_port}/{self.redis_database}"
+
+    def build_rabbitmq_dsn(self) -> str:
+        return f"amqp://{self.rabbitmq_user}:{self.rabbitmq_password}@{self.rabbitmq_host}:{self.rabbitmq_port}//"
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = False
+
+
+settings = Settings()
