c142a9f517186e006372b004999cd47e49403560
Mark Shamanskii <darkweid713@gmail.com>
2025-05-27 13:01:01 +0500

    Feat: Update UUID handling in models and set classes as abstract
    Refactor UUID import for clarity and establish base model as abstract for better inheritance.

--- app/core/database/models.py
+++ app/core/database/models.py
@@ -1,8 +1,9 @@
 import uuid
 from datetime import datetime
+from uuid import UUID as PY_UUID
 
 from sqlalchemy import MetaData, func, DateTime, Integer
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 
@@ -24,6 +25,7 @@ class TimestampMixin:
     created_at: DateTime, oncreate trigger
     updated_at: DateTime, onupdate trigger
     """
+    __abstract__ = True
 
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),
@@ -35,8 +37,10 @@ class UUIDIDMixin:
     Add a UUID column to a mapped class
     id: UUID
     """
-    id: Mapped[UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    __abstract__ = True
+
+    id: Mapped[PY_UUID] = mapped_column(
+        PG_UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
 
 
@@ -45,6 +49,8 @@ class IntegerIDMixin:
     Add an autoincrement column to a mapped class
     id: Integer
     """
+    __abstract__ = True
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
 
@@ -54,6 +60,8 @@ class SoftDeleteMixin:
     deleted_at: DateTime
     is_deleted: Boolean
     """
+    __abstract__ = True
+
     deleted_at: Mapped[datetime | None] = mapped_column(
         DateTime(timezone=True), nullable=True, default=None
     )
