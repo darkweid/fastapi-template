1d12f1092aa1cf0802fc57785d63f514e22e4900
Mark Shamanskii <darkweid713@gmail.com>
2025-06-19 12:42:35 +0500

    Feat: Add support for null filters in service methods for improved flexibility

--- app/core/services.py
+++ app/core/services.py
@@ -23,21 +23,40 @@ class BaseService(Generic[T, CreateSchema, UpdateSchema, RepoType]):
         """Create a new record using the provided session."""
         return await self.repository.create(session, data.model_dump())
 
-    async def get_single(self, session: AsyncSession, **filters: Any) -> Optional[T]:
-        return await self.repository.get_single(session, **filters)
+    async def get_single(self,
+                         session: AsyncSession,
+                         allow_null_filters: bool = False,
+                         **filters: Any) -> Optional[T]:
+        return await self.repository.get_single(session, allow_null_filters, **filters)
 
-    async def get_list(self, session: AsyncSession, **filters: Any) -> Page[T]:
+    async def get_list(self,
+                       session: AsyncSession,
+                       allow_null_filters: bool = False,
+                       **filters: Any) -> Page[T]:
         """Retrieve a list of records matching the filters using the provided session."""
-        return await self.repository.get_list(session, **filters)
+        return await self.repository.get_list(session, allow_null_filters, **filters)
 
-    async def get_list_without_pagination(self, session: AsyncSession, **filters: Any) -> List[T]:
+    async def get_list_without_pagination(self,
+                                          session: AsyncSession,
+                                          allow_null_filters: bool = False,
+                                          **filters: Any) -> List[T]:
         """Retrieve a list of records matching the filters using the provided session."""
-        return await self.repository.get_list_without_pagination(session, **filters)
+        return await self.repository.get_list_without_pagination(session, allow_null_filters, **filters)
 
-    async def update(self, session: AsyncSession, data: UpdateSchema, **filters: Any) -> Optional[T]:
+    async def update(self,
+                     session: AsyncSession,
+                     data: UpdateSchema,
+                     allow_null_filters: bool = False,
+                     **filters: Any) -> Optional[T]:
         """Update a record matching the filters using the provided session."""
-        return await self.repository.update(session, data.model_dump(exclude_unset=True), **filters)
+        return await self.repository.update(session,
+                                            data.model_dump(exclude_unset=True),
+                                            allow_null_filters,
+                                            **filters)
 
-    async def delete(self, session: AsyncSession, **filters: Any) -> Optional[T]:
+    async def delete(self,
+                     session: AsyncSession,
+                     allow_null_filters: bool = False,
+                     **filters: Any) -> Optional[T]:
         """Delete a record matching the filters using the provided session."""
-        return await self.repository.delete(session, **filters)
+        return await self.repository.delete(session, allow_null_filters, **filters)
