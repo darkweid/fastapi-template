16001cc01d5855a635e2c7d81121ad6420d8fc38
Mark Shamanskii <darkweid713@gmail.com>
2025-11-07 16:35:26 +0500

    Refactor: Enhance PostGIS initialization script to conditionally create pg_stat_statements extension

--- postgres/init-postgis.sh
+++ postgres/init-postgis.sh
@@ -4,6 +4,19 @@ set -e
 # Activate extensions
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS postgis;
-    CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+    DO \$\$
+    BEGIN
+        IF EXISTS (
+            SELECT 1
+            FROM pg_settings
+            WHERE name = 'shared_preload_libraries'
+              AND position('pg_stat_statements' in setting) > 0
+        ) THEN
+            EXECUTE 'CREATE EXTENSION IF NOT EXISTS pg_stat_statements';
+        ELSE
+            RAISE NOTICE 'Skipping pg_stat_statements extension; shared_preload_libraries is missing it.';
+        END IF;
+    END;
+    \$\$;
 
 EOSQL
