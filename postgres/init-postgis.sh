bfbd7365fc06e88326e6df3e5c98742a44adf3be
Mark Shamanskii <darkweid713@gmail.com>
2025-11-07 13:41:16 +0500

    Refactor: Activate pg_stat_statements extension in PostGIS initialization script

--- postgres/init-postgis.sh
+++ postgres/init-postgis.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-# Activate PostGIS extension
+# Activate extensions
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
 EOSQL
